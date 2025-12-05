3ac42834d70fa06d41073d5c31bbe9f52176c314
strangka <strangka@student.21-school.ru>
2025-12-06 00:08:59 +0700

    push

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,3 @@
-// playwright.config.js (упрощенный)
 const { defineConfig, devices } = require('@playwright/test')
 
 module.exports = defineConfig({
@@ -9,7 +8,6 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:5000',
     trace: 'on-first-retry',
   },
   projects: [
